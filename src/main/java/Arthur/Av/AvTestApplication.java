1fcc6ada2b1c550a63cf3174f95b2f1f5740780c
Ghost <Ghost@DESKTOP-UIQ65IE>
2023-04-05 01:41:36 -0300

    Commit inicial Avaliação

---
+++ src/main/java/Arthur/Av/AvTestApplication.java
@@ -0,0 +1,13 @@
+package Arthur.Av;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Arthur/Av/AvTestApplicationTests.java
@@ -0,0 +1,13 @@
+package Arthur.Av;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
